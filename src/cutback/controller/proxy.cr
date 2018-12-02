efa73177f1aa90540b16a34730ac49c203045995
Ryan Scott Lewis <ryanscottlewis@gmail.com>
2018-12-02 15:56:01 -0500

    Controllers: proxy#verify delegates to checksum#verify

--- src/cutback/controller/proxy.cr
+++ src/cutback/controller/proxy.cr
@@ -1,9 +1,10 @@
 class Cutback::Controller::Proxy < Cutback::Controller
 
-  actions help, version, create, read, update, destroy
+  actions help, version, create, read, update, destroy, verify
 
   delegate_action help, version, to: application
   delegate_action create, read, update, destroy, to: backup
+  delegate_action verify, to: checksum
 
 end
 
