be9ccbb7e9964928ec1edc75d7ebab760f5bfac3
Ryan Scott Lewis <ryanscottlewis@gmail.com>
2018-12-03 05:01:02 -0500

    Docs: Add second usage line signaling a single argument given is the ACTION not CONTROLLER

--- src/generate/formatter/help/usage.cr
+++ src/generate/formatter/help/usage.cr
@@ -13,6 +13,16 @@ class Generate::Formatter::Help::Usage < Generate::Formatter
       io << argument.name.upcase
       io << "]"
     end
+
+    io << "\n".ljust(8)
+
+    io << @root.name
+
+    [@root.arguments[0], @root.arguments[2]].each do |argument|
+      io << " ["
+      io << argument.name.upcase
+      io << "]"
+    end
   end
 
 end
--- src/generate/formatter/man/usage.cr
+++ src/generate/formatter/man/usage.cr
@@ -13,6 +13,16 @@ class Generate::Formatter::Man::Usage < Generate::Formatter
       io << argument.name.upcase.to_roff.italic
       io << "]"
     end
+
+    io << "\n.br\n"
+
+    io << @root.name.to_roff.bold
+
+    [@root.arguments[0], @root.arguments[2]].each do |argument|
+      io << " ["
+      io << argument.name.upcase.to_roff.italic
+      io << "]"
+    end
   end
 
 end
