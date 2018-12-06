3b778cd48b9c2809b2c68d4e35df0054e01ba144
Ryan Scott Lewis <ryanscottlewis@gmail.com>
2018-12-06 13:57:15 -0500

    Controllers: Add proxy actions for most controllers

--- src/cutback/controller/proxy.cr
+++ src/cutback/controller/proxy.cr
@@ -1,11 +1,18 @@
 class Cutback::Controller::Proxy < Cutback::Controller
 
-  actions help, version, create, read, update, destroy, metadata, verify
+  actions help, version, create, read, update, destroy, verify,
+          manifest, records, archive, checksum, metadata, config, backup
 
   delegate_actions help, version,                 to: application
   delegate_actions create, read, update, destroy, to: backup
-  delegate_action  metadata,                      to: metadata, action: read
   delegate_actions verify,                        to: checksum
+  delegate_action  manifest,                      to: manifest, action: create
+  delegate_action  records,                       to: records,  action: create
+  delegate_action  archive,                       to: archive,  action: create
+  delegate_action  checksum,                      to: checksum, action: create
+  delegate_action  metadata,                      to: metadata, action: create
+  delegate_action  config,                        to: config,   action: create
+  delegate_action  backup,                        to: backup,   action: read
 
 end
 
