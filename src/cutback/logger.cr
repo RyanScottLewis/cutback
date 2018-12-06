f2815e41d350c1ede6ce934fc78137dbe87ee483
Ryan Scott Lewis <ryanscottlewis@gmail.com>
2018-12-06 16:00:16 -0500

    Logger: Convert logger into an interface to a set of loggers, since IO::MultiWriter seems borked

--- src/cutback/logger.cr
+++ src/cutback/logger.cr
@@ -1,28 +1,41 @@
-class Cutback::Logger < Logger
+class Cutback::Logger
 
-  FORMATTER = Formatter.new do |severity, datetime, progname, message, io|
+  FORMATTER = ::Logger::Formatter.new do |severity, datetime, progname, message, io|
     label = severity.unknown? ? "ANY" : severity.to_s
     io << "[" << datetime << ", " << label.rjust(5) << "] "
     io << message
   end
 
+  @loggers = {} of Symbol => ::Logger
   @options : Options
   @paths   : PathList
 
   def initialize(@options, @paths)
-    super(STDOUT, Severity::DEBUG, FORMATTER)
+    @loggers[:stdout] = ::Logger.new(STDOUT, ::Logger::Severity::DEBUG, FORMATTER)
+    @loggers[:stderr] = ::Logger.new(STDERR, ::Logger::Severity::FATAL, FORMATTER)
   end
 
-  # TODO: BROKEN! And let me explain how...
-  #
-  #   @io = file                              # Works fine
-  #   @io = STDOUT                            # Works fine
-  #   @io = IO::MultiWriter.new(STDOUT, file) # Broken
-  #
-  # And it is definitely somehow maybe MultiWriter's fault
   def update
     file = @paths.log.open("a+")
-    @io  = IO::MultiWriter.new(STDOUT, file)
+
+    @loggers[:file] = ::Logger.new(file, ::Logger::Severity::DEBUG, FORMATTER)
+  end
+
+  macro def_severity(name)
+    def {{name.id}}(message)
+      log(::Logger::Severity::{{name.id.upcase}}, message)
+    end
+  end
+
+  def_severity debug
+  def_severity info
+  def_severity warn
+  def_severity error
+  def_severity fatal
+  def_severity unknown
+
+  def log(severity, message)
+    @loggers.values.each(&.log(severity, message))
   end
 
 end
