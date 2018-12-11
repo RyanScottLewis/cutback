1df02e185d018a8bd6a777710d21d20600518b11
Ryan Scott Lewis <ryanscottlewis@gmail.com>
2018-12-11 18:31:24 -0500

    Definitions: Allow Option to have default value

--- src/cutback/definition/option.cr
+++ src/cutback/definition/option.cr
@@ -1,10 +1,13 @@
 class Cutback::Definition::Option < Cutback::Definition
 
+  alias Value = Bool | Int32 | String | Time | Array(String)
+
   mapping(
     name:        String,
     description: String,
-    short:       String,
+    short:       String?,
     type:        String,
+    default:     Value?,
   )
 
   formatters help, man
