ec9d84576d114600051306aecac370a19e833655
Ryan Scott Lewis <ryanscottlewis@gmail.com>
2018-12-11 20:47:27 -0500

    Resource: Check for --dry when creating config resource

--- src/cutback/controller/resource/config.cr
+++ src/cutback/controller/resource/config.cr
@@ -3,6 +3,8 @@ class Cutback::Controller::Resource::Config < Cutback::Controller::Resource
   actions create, read, update, destroy
 
   protected def create_resource
+    return if @options.dry
+
     config = Cutback::Config.new(@options)
 
     config.save(@paths.config, @options.format)
