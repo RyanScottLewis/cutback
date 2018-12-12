d96987bf55d0b9978bafa86c044389d388b16af4
Ryan Scott Lewis <ryanscottlewis@gmail.com>
2018-12-12 15:58:55 -0500

    Application: Create macros for executing processors

--- src/cutback/application.cr
+++ src/cutback/application.cr
@@ -17,18 +17,16 @@ class Cutback::Application
     @router      = Router.new(@controllers, @logger)
   end
 
+  macro process(name, *arguments)
+    Processor::{{name.id.camelcase}}.execute!(@logger, {{*arguments}})
+  end
+
+  macro method_missing(call)
+    process({{call.name}}, {{*call.args}})
+  end
+
   def execute
-    Processor::OptionsParser.execute!(@logger, @arguments, @options)
-    Processor::OptionsValidator.execute!(@logger, @options)
-    Processor::ArgumentsPreprocessor.execute!(@logger, @arguments)
-    Processor::ArgumentsValidator.execute!(@logger, @arguments)
-    Processor::RouterArgumentsUpdater.execute!(@logger, @arguments, @router)
-    Processor::RouterValidator.execute!(@logger, @router)
-    Processor::IdentifierOptionsUpdater.execute!(@logger, @identifier, @options)
-    Processor::PathsOptionsUpdater.execute!(@logger, @paths, @tools, @identifier, @options)
-    Processor::ToolsOptionsUpdater.execute!(@logger, @tools, @options)
-    Processor::ControllerFactory.execute!(@logger, @app, @options, @identifier, @paths, @tools, @controllers, @router)
-    Processor::LoggerOutputUpdater.execute!(@logger, @paths, @options)
+    execute_processors
 
     @router.execute
   rescue error : Cutback::Error
@@ -39,6 +37,20 @@ class Cutback::Application
     @logger.fatal("Error: #{error.class} - #{error}")
 
     exit 1
+  end
+
+  protected def execute_processors
+    options_parser(@arguments, @options)
+    options_validator(@options)
+    arguments_preprocessor(@arguments)
+    arguments_validator(@arguments)
+    router_arguments_updater(@arguments, @router)
+    router_validator(@router)
+    identifier_options_updater(@identifier, @options)
+    paths_options_updater(@paths, @tools, @identifier, @options)
+    tools_options_updater(@tools, @options)
+    controller_factory(@app, @options, @identifier, @paths, @tools, @controllers, @router)
+    logger_output_updater(@paths, @options)
   end
 
 end
