917aa52878823a5e6127643e7d4176ec74d6de36
Ryan Scott Lewis <ryanscottlewis@gmail.com>
2018-12-12 15:29:36 -0500

    Processors: Move Executable helper code into Processor since it's the only thing that uses it

--- src/cutback/processor.cr
+++ src/cutback/processor.cr
@@ -1,6 +1,31 @@
 abstract class Cutback::Processor
 
-  include Helpers::Executable
+  def self.execute(*arguments)
+    new(*arguments).execute
+  end
+
+  def self.execute!(logger, *arguments)
+    new(*arguments).execute!(logger)
+  end
+
+  abstract def execute
+
+  def execute!(logger : Cutback::Logger)
+    log_execution(logger)
+    execute
+  end
+
+  protected def log_execution(logger)
+    logger.debug("%s - %s" % [logger_scope, logger_message])
+  end
+
+  protected def logger_scope
+    self.class.name.gsub(/^Cutback::/, "")
+  end
+
+  protected def logger_message
+    "Execute"
+  end
 
 end
 
