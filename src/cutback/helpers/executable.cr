1b1922db6de41f7b96c2eed8fd536f5dd4044ffa
Ryan Scott Lewis <ryanscottlewis@gmail.com>
2018-12-11 22:20:43 -0500

    Helpers: Add logger message helpers to Executable

--- src/cutback/helpers/executable.cr
+++ src/cutback/helpers/executable.cr
@@ -19,11 +19,20 @@ module Cutback::Helpers::Executable
   abstract def execute
 
   def execute!(logger : Cutback::Logger)
-    message = self.class.name.gsub(/^Cutback::/, "").gsub(/::/, ": ")
-
-    logger.debug(message)
-
+    log_execution(logger)
     execute
+  end
+
+  protected def log_execution(logger)
+    logger.debug("%s - %s" % [logger_scope, logger_message])
+  end
+
+  protected def logger_scope
+    self.class.name.gsub(/^Cutback::/, "")
+  end
+
+  protected def logger_message
+    "Execute"
   end
 
 end
