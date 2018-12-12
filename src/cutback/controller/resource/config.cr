83b96296b45a68db0784bea522f967d1f8621c37
Ryan Scott Lewis <ryanscottlewis@gmail.com>
2018-12-11 21:36:09 -0500

    Controllers: Add config#view

--- src/cutback/controller/resource/config.cr
+++ src/cutback/controller/resource/config.cr
@@ -1,6 +1,12 @@
 class Cutback::Controller::Resource::Config < Cutback::Controller::Resource
 
-  actions create, read, update, destroy
+  actions create, read, update, destroy, view
+
+  def view
+    config = Cutback::Config.new(@options)
+
+    puts config.dump(@options.format)
+  end
 
   protected def create_resource
     return if @options.dry
